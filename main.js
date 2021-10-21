1a2ad1c1b183d0105fbda5f21ec182658f169c74
Anna Trubnikova <47085312+trubo4ka911@users.noreply.github.com>
2021-10-21 11:42:56 +0300

    Initial

---
+++ main.js
@@ -0,0 +1,52 @@
+"use strict";
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+  start() {
+    const expectDay = this.targetDate.getTime();
+
+    const day = document.querySelector(`${this.selector} [data-value="days"]`);
+    const hour = document.querySelector(
+      `${this.selector} [data-value="hours"]`
+    );
+    const minute = document.querySelector(
+      `${this.selector} [data-value="mins"]`
+    );
+    const second = document.querySelector(
+      `${this.selector} [data-value="secs"]`
+    );
+
+    setInterval(() => {
+      const currentTime = new Date().getTime();
+      const deltaTime = expectDay - currentTime;
+
+      const days = pad(Math.floor(deltaTime / (1000 * 60 * 60 * 24)));
+      const hours = pad(
+        Math.floor((deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      );
+      const mins = pad(
+        Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60))
+      );
+      const secs = pad(Math.floor((deltaTime % (1000 * 60)) / 1000));
+
+      day.textContent = days;
+      hour.textContent = hours;
+      minute.textContent = mins;
+      second.textContent = secs;
+    }, 1000);
+  }
+}
+
+const myTimer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Jul 17, 2022"),
+});
+
+myTimer.start();
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
